10209743ee9c03b0be0047af6cd15e597c66b14b
user436 <nyvia.projects@gmail.com>
2023-02-27 23:21:45 -0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import * as net from 'net';
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Enter your name: ', (answer) => {
+  console.log(`Hello, ${answer}!`);
+  rl.close();
+});
+
+console.log('hello');
