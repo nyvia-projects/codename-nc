b088d23a1671fe7e8b241e6a0b7b9174094162b7
user436 <nyvia.projects@gmail.com>
2023-02-28 00:31:40 -0800

    minor fix

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import * as net from 'net';
+
 import * as readline from 'readline';
 
 const rl = readline.createInterface({
   input: process.stdin,
+
   output: process.stdout,
 });
 
 rl.question('Enter your name: ', (answer) => {
   console.log(`Hello, ${answer}!`);
+
   rl.close();
 });
