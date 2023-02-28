7f4314e5ff02cd90ec225c4b87935d1f33f9f29d
user436 <nyvia.projects@gmail.com>
2023-02-28 02:27:01 -0800

    added some CI jobs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,14 +3,22 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
   ],
+  overrides: [
+    {
+      files: '**/*.test.*',
+      rules: {
+        '@typescript-eslint/no-unsafe-call': 'off',
+      },
+    },
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
-  root: true,
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': ['error'],
+  parserOptions: {
+    tsconfigRootDir: '.',
+    project: ['./tsconfig.json'],
   },
+  plugins: ['@typescript-eslint'],
+  root: true,
 };
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import * as net from 'net';
-
 import * as readline from 'readline';
 
 const rl = readline.createInterface({
   input: process.stdin,
-
   output: process.stdout,
 });
 
 rl.question('Enter your name: ', (answer) => {
-  console.log(`
-  Hello, ${answer}!
-  `);
-
+  console.log(`Hello, ${answer}!`);
   rl.close();
 });
+
+(name = 'ann') => {
+  if (name) {
+    console.log(name);
+  }
+};
